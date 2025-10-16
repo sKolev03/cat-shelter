8ffae0a6e6dd7960f336504f74441001cbf44a67
sKolev03 <stiliyankolev03@gmail.com>
2025-10-16 20:31:50 +0300

    Show Home Page

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ SoftUni JS Back-End Course Project
  - [x] Dev script
  - [x] Debugging
  - [x] Add basic http server
+ - [x] Add rescoures
+ - [x] Show Home Page
+ - [x] Add css
+ - [x] Extract css and htmlHomePage into separete js files
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import http from 'http';
 
+import homeHtml from './home.html.js';
+import siteCss from './site.css.js';
+
 const server = http.createServer((req, res) => {
-    res.write('Hello World!');
+    if (req.url === '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+        });
+
+        res.write(homeHtml);
+
+    } else if (req.url === '/styles/site.css') {
+        res.writeHead(200, {
+            'Content-Type': 'text/css',
+        });
+
+        res.write(siteCss);
+    }
 
     res.end();
 });
 
 server.listen(5000);
 
-console.log('Server is listening on http://localhost:5000...');
+console.log('Server is listening on http://localhost:5000');
