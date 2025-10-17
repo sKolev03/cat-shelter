99efa5133ab35c69fede7a9cc9e9e8acc8a77a99
sKolev03 <stiliyankolev03@gmail.com>
2025-10-17 03:58:34 +0300

    show dynamic cats on home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import http from 'http';
 import fs from 'fs/promises';
 
+import cats from './cats.js';
+
 const server = http.createServer(async (req, res) => {
     let html = '';
 
@@ -43,7 +45,10 @@ function readFile(path) {
 async function homeView() {
     const homeHtml = await readFile('./src/views/home/index.html');
 
-    return homeHtml;
+    const catsHtml = cats.map(cat => catTemplate(cat)).join('\n');
+    const result = homeHtml.replace('{{cats}}', catsHtml);
+
+    return result;
 }
 
 async function addBreedView() {
@@ -56,6 +61,22 @@ async function addCatView(){
     const html = await readFile('./src/views/addCat.html');
     
     return html;
+}
+
+function catTemplate(cat) {
+    return `
+    <li>
+        <img src="${cat.imageUrl}" alt="${cat.name}">
+        <h3>${cat.name}</h3>
+        <p><span>Price: </span>${cat.price}$</p>
+        <p><span>Breed: </span>${cat.breed}</p>
+        <p><span>Description: </span>${cat.description}</p>
+        <ul class="buttons">
+            <li class="btn edit"><a href="">Change Info</a></li>
+            <li class="btn delete"><a href="">New Home</a></li>
+         </ul>
+    </li>
+`;
 }
 
 server.listen(5000);
